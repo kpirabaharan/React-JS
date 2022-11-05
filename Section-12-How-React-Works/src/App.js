511f0fde46008df16af4013741576fd731bf5038
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-05 18:03:08 -0400

    Section 12: Added useCallback()
    useCallback() only updates whatever is in the function when its changed,
    it compares itself to previous version to see if there is a change as it
    saves previous version in memory. Has dependencies, check comments.
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-12-How-React-Works/src/App.js
+++ Section-12-How-React-Works/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import './App.css';
 import DemoOutput from './components/Demo/DemoOutput';
@@ -6,17 +6,28 @@ import Button from './components/UI/Button/Button';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('App Running');
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   };
+
+  // If allowToggle is not set as a dependancy, useCallback prevents
+  // toggleParagraphHandler from updating since it was initially created,
+  // It thinks allowToggle is still false. 
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
