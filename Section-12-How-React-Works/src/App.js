aab21dbdac5eb10f532fba698471ab6fff9dd3f1
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-05 16:34:34 -0400

    Section 12: Child Component Re-evaluation
    Child components re-evaluate when parent state changes and calls child
    component.
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-12-How-React-Works/src/App.js
+++ Section-12-How-React-Works/src/App.js
@@ -1,22 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
-import Button from "./components/UI/Button/Button";
+import DemoOutput from './components/Demo/DemoOutput';
+import Button from './components/UI/Button/Button';
 
 function App() {
-    const [showParagraph, setShowParagraph] = useState(false);
+  const [showParagraph, setShowParagraph] = useState(false);
 
-    console.log('App Running')
+  console.log('App Running');
 
-    const toggleParagraphHandler = () => {
-        setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-    }
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
 
-    return (<div className="app">
-            <h1>Hi there!</h1>
-            <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
-            {showParagraph && <p>This is new!</p>}
-        </div>);
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+    </div>
+  );
 }
 
 export default App;
