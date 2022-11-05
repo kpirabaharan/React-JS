4f135f11820a2a091ee95bcd14f094e776036cb0
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-05 19:10:41 -0400

    Section 14: Modified Directory for Project
    Added new files provided by Max.
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-14-Sending-HTTP-Requests/src/App.js
+++ Section-14-Sending-HTTP-Requests/src/App.js
@@ -1,6 +1,7 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -9,13 +10,10 @@ function App() {
   const [error, setError] = useState(null);
 
   const fetchMoviesHandler = useCallback(async () => {
-    // How to send http requests using fetch()
     setIsLoading(true);
     setError(null);
-
     try {
       const response = await fetch('https://swapi.dev/api/films/');
-
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
@@ -30,7 +28,6 @@ function App() {
           releaseDate: movieData.release_date,
         };
       });
-      console.log(transformedMovies.title);
       setMovies(transformedMovies);
     } catch (error) {
       setError(error.message);
@@ -41,6 +38,10 @@ function App() {
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
 
   let content = <p>Found no movies.</p>;
 
@@ -58,6 +59,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
