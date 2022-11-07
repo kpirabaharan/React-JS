00f024894cacfb8fe3442d05f9f158cb68b4f99f
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-06 20:46:42 -0500

    Section 15: Added useCallback to fix Previous Error
    Wrapping the sendRequest function in a useCallback fixed the error and
    app is now loading.
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-15-Custom-Hooks/src/hooks/use-http.js
+++ Section-15-Custom-Hooks/src/hooks/use-http.js
@@ -1,17 +1,17 @@
-import useState from 'react';
+import { useState, useCallback } from 'react';
 
 const useHttp = (requestConfig, applyData) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async () => {
+  const sendRequest = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(requestConfig.url, {
         method: requestConfig.method ? requestConfig.method : 'GET',
         headers: requestConfig.headers ? requestConfig.headers : {},
-        body: JSON.stringify(requestConfig.body),
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
 
       if (!response.ok) {
@@ -25,7 +25,8 @@ const useHttp = (requestConfig, applyData) => {
       setError(err.message || 'Something went wrong!');
     }
     setIsLoading(false);
-  };
+  }, []);
+
   return {
     isLoading,
     error,
