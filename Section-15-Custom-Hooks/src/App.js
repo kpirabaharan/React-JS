bba5e676c5d6e98b9389b7b82db2d12ff7efa32a
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-06 20:42:06 -0500

    Section 15: Using custom hook in place of fetch
    "DOES NOT WORK". Replaced the normal fetch task with the custom hook that was created.
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-15-Custom-Hooks/src/App.js
+++ Section-15-Custom-Hooks/src/App.js
@@ -1,43 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-course-custom-hook-6450f-default-rtdb.firebaseio.com/tasks.json',
-      );
+  const transformTasks = (tasksObj) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
     }
-    setIsLoading(false);
+
+    setTasks(loadedTasks);
   };
+
+  // sendRequest: fetchTasks is destrucuring what is returned from the useHttp
+  // hook and names it as fetchTasks
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(
+    {
+      url: 'https://react-course-custom-hook-6450f-default-rtdb.firebaseio.com/tasks.json',
+    },
+    transformTasks,
+  );
 
   useEffect(() => {
     fetchTasks();
-  }, []);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
