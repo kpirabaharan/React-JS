0fb632636a76af1ec3ed4b89e21d167eebe7a95a
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-06 21:01:09 -0500

    Section 15: Refactored Code - No more Dependencies in useHttp
    Removed dependencies from hook and added it to the function in the hook.
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-15-Custom-Hooks/src/App.js
+++ Section-15-Custom-Hooks/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -7,31 +7,24 @@ import useHttp from './hooks/use-http';
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (tasksObj) => {
-    const loadedTasks = [];
-
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
-  // sendRequest: fetchTasks is destrucuring what is returned from the useHttp
-  // hook and names it as fetchTasks
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    {
-      url: 'https://react-course-custom-hook-6450f-default-rtdb.firebaseio.com/tasks.json',
-    },
-    transformTasks,
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+    fetchTasks(
+      {
+        url: 'https://react-course-custom-hook-6450f-default-rtdb.firebaseio.com/tasks.json',
+      },
+      transformTasks,
+    );
   }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
--- Section-15-Custom-Hooks/src/hooks/use-http.js
+++ Section-15-Custom-Hooks/src/hooks/use-http.js
@@ -1,10 +1,10 @@
 import { useState, useCallback } from 'react';
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = useCallback(async () => {
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
     try {
