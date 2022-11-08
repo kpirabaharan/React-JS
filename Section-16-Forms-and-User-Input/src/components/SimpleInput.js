6d053642582b8f6d206af03fdbefbbe8f56e794a
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-07 19:49:39 -0500

    Section 16: Saving and Submitting Forms Using useRef
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-16-Forms-and-User-Input/src/components/SimpleInput.js
+++ Section-16-Forms-and-User-Input/src/components/SimpleInput.js
@@ -1,23 +1,20 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 
 const SimpleInput = (props) => {
-  const [enteredName, setEnteredName] = useState('');
-
-  const nameInputChangeHandler = (event) => {
-    setEnteredName(event.target.value);
-  };
+  const nameInputRef = useRef();
 
   const formSubmissionHandler = (event) => {
-    // To prevent http request from being sent add event.preventDefault();
     event.preventDefault();
-    console.log(enteredName);
+    // Ref Method
+    const inputVal = nameInputRef.current.value;
+    console.log(inputVal);
   };
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={nameInputChangeHandler} />
+        <input ref={nameInputRef} type="text" id="name" />
       </div>
       <div className="form-actions">
         <button>Submit</button>
