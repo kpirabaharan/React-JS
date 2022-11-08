183f731ffd713dbd5fc8eba67456c13ba9f86ebd
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-07 19:45:01 -0500

    Section 16: Saving and Submitting Forms Using useState
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-16-Forms-and-User-Input/src/components/SimpleInput.js
+++ Section-16-Forms-and-User-Input/src/components/SimpleInput.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    // To prevent http request from being sent add event.preventDefault();
+    event.preventDefault();
+    console.log(enteredName);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
