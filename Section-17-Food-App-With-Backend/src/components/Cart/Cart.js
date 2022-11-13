2f1d6b7d54c1170d4672ecf886450f7b37e74ae9
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-13 15:19:25 -0500

    Section 17: Added Checkout Form
    Modal shows checkout form when order is submitted.
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-17-Food-App-With-Backend/src/components/Cart/Cart.js
+++ Section-17-Food-App-With-Backend/src/components/Cart/Cart.js
@@ -1,22 +1,28 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+  const [isCheckout, setIsCheckout] = useState(false);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
-  
+
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem({...item, amount: 1})
+    cartCtx.addItem({ ...item, amount: 1 });
   };
 
   const cartItemRemoveHandler = (id) => {
     cartCtx.removeItem(id);
+  };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
   };
 
   const cartItems = (
@@ -34,6 +40,19 @@ const Cart = (props) => {
     </ul>
   );
 
+  const checkoutForm = (
+    <div className={classes.actions}>
+      <button className={classes['button--alt']} onClick={props.onHideCart}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
   return (
     <Modal onClose={props.onHideCart}>
       {cartItems}
@@ -41,12 +60,8 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onHideCart}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+      {isCheckout && <Checkout onCancel={props.onHideCart}/>}
+      {!isCheckout && checkoutForm}
     </Modal>
   );
 };
--- Section-17-Food-App-With-Backend/src/components/Meals/AvailableMeals.js
+++ Section-17-Food-App-With-Backend/src/components/Meals/AvailableMeals.js
@@ -12,7 +12,7 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        'https://react-course-foodapp-backend-default-rtdb.firebaseio.com/meals',
+        'https://react-course-foodapp-backend-default-rtdb.firebaseio.com/meals.json',
       );
 
       // Throwing a new error prevents remaining code from executing
