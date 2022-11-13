3fb3fae8dc28e46c98739ae0ddae0016501b218c
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-13 00:19:41 -0500

    Section 17: Added Loading State
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-17-Food-App-With-Backend/src/components/Meals/AvailableMeals.js
+++ Section-17-Food-App-With-Backend/src/components/Meals/AvailableMeals.js
@@ -6,6 +6,7 @@ import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -31,6 +32,7 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
     fetchMeals();
   }, []);
@@ -44,6 +46,13 @@ const AvailableMeals = () => {
       price={meal.price}
     />
   ));
+
+  if(isLoading)
+  {
+    return <section className={classes.MealsLoading}>
+      <p>Loading...</p>
+    </section>
+  }
 
   return (
     <section className={classes.meals}>
