4f510e5781862fe4b30a7c7b53d804cb28ef7980
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-15 23:00:00 -0500

    Section 18: Added Counter with Buttons to Change it
    Uses useSelector and useDispatch to get access to redux states.
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-18-Redux/Project/src/components/Counter.js
+++ Section-18-Redux/Project/src/components/Counter.js
@@ -1,12 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch({
+      type: 'increment',
+    });
+  };
+
+  const decrementHandler = () => {
+    dispatch({
+      type: 'decrement',
+    });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
