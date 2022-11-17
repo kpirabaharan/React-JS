9ed66766f009e234060195b2894f3ca2f3be680d
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-16 23:37:44 -0500

    Section 18: Dynamic Action
    Can change increment value dynamically.
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-18-Redux/Project/src/components/Counter.js
+++ Section-18-Redux/Project/src/components/Counter.js
@@ -7,15 +7,10 @@ const Counter = () => {
 
   const toggleCounterHandler = () => {};
 
-  const incrementHandler = () => {
+  const incrementHandler = (value) => {
     dispatch({
       type: 'increment',
-    });
-  };
-
-  const decrementHandler = () => {
-    dispatch({
-      type: 'decrement',
+      value,
     });
   };
 
@@ -24,8 +19,12 @@ const Counter = () => {
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={() => incrementHandler(1)}>Increment</button>
+        <button onClick={() => incrementHandler(-1)}>Decrement</button>
+      </div>
+      <div>
+        <button onClick={() => incrementHandler(5)}>Increment by 5</button>
+        <button onClick={() => incrementHandler(-5)}>Decrement by 5</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- Section-18-Redux/Project/src/store/index.js
+++ Section-18-Redux/Project/src/store/index.js
@@ -3,11 +3,7 @@ import { createStore } from 'redux';
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === 'increment')
     return {
-      counter: state.counter + 1,
-    };
-  if (action.type === 'decrement')
-    return {
-      counter: state.counter - 1,
+      counter: state.counter + action.value,
     };
   return state;
 };
