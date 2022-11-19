ac74fc285e359a4c8fe18eb000b1ec0bdee4db18
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-19 13:58:23 -0500

    Section 18: Created Auth Slice
    Multiple slices
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-18-Redux/Project/src/store/index.js
+++ Section-18-Redux/Project/src/store/index.js
@@ -1,10 +1,14 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
+
+const initialAuthState = {
+  isAuthenticated: false,
+};
 
 const counterSlice = createSlice({
   name: 'counter',
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state, action) {
       // Default action parameter is payload
@@ -16,13 +20,25 @@ const counterSlice = createSlice({
   },
 });
 
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 // configureStore is a better way to deal with multiple reducers.
 // createStore is deprecated anyways
 const store = configureStore({
-  // reducer: { counter: counterSlice.reducer }, // For mulitple reducers
-  // We only have one in this scenario
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer }, // For mulitple reducers
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
