f1cd6efbf96d591f813abb8ad10a4ff635081025
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-31 23:02:00 -0500

    Section 19: useEffect with Redux
    Having all logic (heavy) in reducer and use components for side-effects.
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-19-Advanced-Redux/src/App.js
+++ Section-19-Advanced-Redux/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -5,7 +6,17 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    // console.log(cart);
+    fetch(
+      'https://react-advanced-redux-6e18f-default-rtdb.firebaseio.com/cart.json',
+      { method: 'PUT', body: JSON.stringify(cart) },
+    );
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
