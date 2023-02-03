c333213a0d5ea6fcb68a1d83705dce6f563434db
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-02-02 21:47:07 -0500

    Section 20: Defining Routes
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-20-React-Router/src/App.jsx
+++ Section-20-React-Router/src/App.jsx
@@ -1,5 +1,15 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from './routes/Home';
+import ProductsPage from './routes/Products';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <ProductsPage /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
