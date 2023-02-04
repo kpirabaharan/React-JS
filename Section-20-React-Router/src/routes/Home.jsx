5af77893831bcc54f90feb30068d7e168a701883
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-02-04 14:11:33 -0500

    Section 20: Navigate Programmtically with useNavigate
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-20-React-Router/src/routes/Home.jsx
+++ Section-20-React-Router/src/routes/Home.jsx
@@ -1,13 +1,20 @@
 import { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navHandler() {
+    navigate('/products');
+  }
+
   return (
     <Fragment>
       <h1>My Home Page</h1>
       <p>
         Go to <Link to={'/products'}>the list of products</Link>
       </p>
+      <button onClick={navHandler}>Navigate</button>
     </Fragment>
   );
 }
