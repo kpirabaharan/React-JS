6c85dad2aa8b38fa84a8d569bdf2a6461aaa76e6
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-02-03 23:55:18 -0500

    Section 20: Adding Error Page Route
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-20-React-Router/src/App.jsx
+++ Section-20-React-Router/src/App.jsx
@@ -3,11 +3,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './routes/Home';
 import ProductsPage from './routes/Products';
 import RootLayout from './routes/Root';
+import ErrorPage from './routes/Error';
 
 const router = createBrowserRouter([
   {
-    path: '/',
     element: <RootLayout />,
+    path: '/',
+    errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
--- Section-20-React-Router/src/routes/Error.jsx
+++ Section-20-React-Router/src/routes/Error.jsx
@@ -1,17 +1,16 @@
 import React, { Fragment } from 'react';
-import { Outlet } from 'react-router-dom';
-
 import MainNavigation from '../components/MainNavigation';
 
-function RootLayout() {
+function ErrorPage() {
   return (
     <Fragment>
       <MainNavigation />
       <main>
-        <Outlet />
+        <h1>An error occurred!</h1>
+        <p>Could not find this page!</p>
       </main>
     </Fragment>
   );
 }
 
-export default RootLayout;
+export default ErrorPage;
