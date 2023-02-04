9d3b91138dea3ecc30879c015aa3db60972b0d29
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-02-04 14:08:00 -0500

    Section 20: Using NavLinks
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-20-React-Router/src/components/MainNavigation.jsx
+++ Section-20-React-Router/src/components/MainNavigation.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 
@@ -9,10 +9,24 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to={'/'}>Home</Link>
+            <NavLink
+              to={'/'}
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to={'/products'}>Products</Link>
+            <NavLink
+              to={'/products'}
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
