3d47d65de5bd4d5dbf65c044bb42277d3e8143c5
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-02-05 22:14:13 -0500

    Section 20: Added Loading Indicator with useNavigation
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-20-React-Router/Project/frontend/src/routes/Root.jsx
+++ Section-20-React-Router/Project/frontend/src/routes/Root.jsx
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
+  const navigation = useNavigation();
+
   return (
     <Fragment>
       <MainNavigation />
       <main>
+        {navigation.state === 'loading' && <p>Loading...</p>}
         <Outlet />
       </main>
     </Fragment>
