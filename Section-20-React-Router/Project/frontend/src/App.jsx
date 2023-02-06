d4310c233bf63111690796422274c027ffd37559
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-02-05 20:07:58 -0500

    Section 20: useLoaderData to Load Fetched Backend Data
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-20-React-Router/Project/frontend/src/App.jsx
+++ Section-20-React-Router/Project/frontend/src/App.jsx
@@ -41,7 +41,20 @@ function App() {
           path: 'events',
           element: <EventsRootLayout />,
           children: [
-            { index: true, element: <EventsPage /> },
+            {
+              index: true,
+              element: <EventsPage />,
+              loader: async () => {
+                const response = await fetch('http://localhost:8080/events');
+
+                if (!response.ok) {
+                  // ...
+                } else {
+                  const resData = await response.json();
+                  return resData.events;
+                }
+              },
+            },
             { path: ':eventId', element: <EventDetailPage /> },
             { path: 'new', element: <NewEventPage /> },
             { path: ':eventId/edit', element: <EditEventPage /> },
--- Section-20-React-Router/Project/frontend/src/routes/Events.jsx
+++ Section-20-React-Router/Project/frontend/src/routes/Events.jsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
+
+import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const DUMMY_EVENTS = [
-    { id: 'e1', title: 'Event 1' },
-    { id: 'e2', title: 'Event 2' },
-    { id: 'e3', title: 'Event 3' },
-    { id: 'e4', title: 'Event 4' },
-  ];
+  const events = useLoaderData();
 
-  return (
-    <ul>
-      {DUMMY_EVENTS.map((event) => (
-        <li key={event.id}>
-          <Link to={event.id}>{event.title}</Link>
-        </li>
-      ))}
-    </ul>
-  );
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
