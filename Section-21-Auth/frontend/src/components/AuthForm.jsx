132f2fc3585f186abb24dfca8231722f03d982b6
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-02-17 14:42:10 -0500

    Section 21: Query Params for Different Actions on Same Page
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-21-Auth/frontend/src/components/AuthForm.jsx
+++ Section-21-Auth/frontend/src/components/AuthForm.jsx
@@ -1,31 +1,27 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+import { Form, Link, useSearchParams } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
-
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  const [searchParams] = useSearchParams();
+  const isLogin = searchParams.get('mode') === 'login';
 
   return (
     <>
-      <Form method="post" className={classes.form}>
+      <Form method='post' className={classes.form}>
         <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
         <p>
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" required />
+          <label htmlFor='email'>Email</label>
+          <input id='email' type='email' name='email' required />
         </p>
         <p>
-          <label htmlFor="image">Password</label>
-          <input id="password" type="password" name="password" required />
+          <label htmlFor='image'>Password</label>
+          <input id='password' type='password' name='password' required />
         </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
+          <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
             {isLogin ? 'Create new user' : 'Login'}
-          </button>
+          </Link>
           <button>Save</button>
         </div>
       </Form>
