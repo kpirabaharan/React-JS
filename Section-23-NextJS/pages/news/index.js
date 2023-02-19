d688dcbcc200f4bd2e042acf0a90954b9d4f4699
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-02-18 19:06:46 -0500

    Section 23: Added Nested Paths
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ Section-23-NextJS/pages/news/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewsPage = () => {
+  return <h1>News Page!</h1>
+};
+
+export default NewsPage;
---
+++ Section-23-NextJS/pages/news/something-important.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DetailPage = () => {
+  return <h1>Detail Page!</h1>
+};
+
+export default DetailPage;
