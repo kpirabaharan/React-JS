80a982b1e1d45d5ed7fa776f015d16c67e3faf0d
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-06-04 13:20:18 -0400

    Section 27: Basic Types, Aliases, Inference
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-27-TypeScript/src/App.tsx
+++ Section-27-TypeScript/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  function add2(a: number, b: number) {
+    return a + b;
+  }
+  const result = add2(2, 2);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const hobbies = ['Sports', 'Cookies'];
+
+  type Person = {
+    name: string;
+    age: number;
+  };
+
+  let people: Person[];
+
+  people = [
+    {
+      name: 'Keeshigan',
+      age: 24,
+    },
+    {
+      name: 'Biranugan',
+      age: 21,
+    },
+  ];
+
+  console.log(people);
 }
 
-export default App
+export default App;
